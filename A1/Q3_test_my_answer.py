2e9b903a5e55b9a998ea969adcf58e2ff9d3ad68
DESKTOP-HOC4NOO\fedel <fedelejohn7008@gmail.com>
2023-05-26 02:57:27 -0400

    test bulk cases

--- A1/Q3_test_my_answer.py
+++ A1/Q3_test_my_answer.py
@@ -1,8 +1,46 @@
 from Q3_tester_code import *
 
+def helper(buildings: list[Building]):
+    n = len(buildings)
+    i = 0
+    j = n // 2
+
+    # front side
+    while (i < n // 2 and j < n):
+        if (buildings[i].coordinates[0] < buildings[j].coordinates[0]):
+            buildings[i].dominance = n - j
+            i = i + 1
+        elif (buildings[i].coordinates[0] > buildings[j].coordinates[0]):
+            j = j + 1
+    i = n - 1
+    j = n // 2 - 1
+    # back side
+    while (i >= n // 2 and j >= 0):
+        if (buildings[i].coordinates[0] < buildings[j].coordinates[0]):
+            j = j - 1
+        elif (buildings[i].coordinates[0] > buildings[j].coordinates[0]):
+            buildings[i].dominance = j + 1
+            i = i - 1
+
+    return buildings
+
 def my_solution(buildings: list[Building]) -> None:
-    pass
+    # reset building dominace to 0
+    for building in buildings:
+        if building.dominance is None:
+            building.dominance = 0
+
+    buildings.sort(key=lambda x: x.coordinates[1])
+    mid_point = len(buildings) // 2
+    left_building = buildings[:mid_point]
+    right_building = buildings[mid_point:]
+
+    left_building.sort(key=lambda x: x.coordinates[0])
+    right_building.sort(key=lambda x: x.coordinates[0])
+    buildings = left_building + right_building
+    buildings = helper(buildings)
+    return buildings
 
 if __name__ == '__main__':
-    test(my_solution)
-    test_bulk(my_solution, 5)
+    # test(my_solution, map_size = 7)
+    test_bulk(my_solution, number_of_random_tries = 5, map_size = 10)
--- A1/Q3_tester_code.py
+++ A1/Q3_tester_code.py
@@ -92,8 +92,12 @@ def test(my_answer: Callable[[list[Building]], None], map_size = 7) -> None:
     Building.print_dominance(buildings_my_solution)
 
 def test_bulk(my_answer: Callable[[list[Building]], None], number_of_random_tries: int, map_size = 100) -> None:
+    total_fail = 0
     for trial in range(number_of_random_tries):
-        print(f'TRIAL: {trial} (Randomly generated map size: {map_size})')
+        print('-------------------------------------------------------------------------------')
+        f_count = 0
+        total_count = 0
+        print(f'TRIAL: {trial + 1} (Randomly generated map size: {map_size})')
         buildings_solution = Building.create_random_buildings(map_size)
         buildings_my_solution = []
         for i in range(len(buildings_solution)):
@@ -105,8 +109,16 @@ def test_bulk(my_answer: Callable[[list[Building]], None], number_of_random_trie
         buildings_my_solution.sort(key=lambda x: x.id)
 
         for i in range(len(buildings_solution)):
+            total_count += 1
             if (buildings_my_solution[i].dominance != buildings_solution[i].dominance):
+                f_count += 1
                 print(f'FAILED: ({buildings_solution[i].coordinates[0]}, {buildings_solution[i].coordinates[1]}) : {buildings_my_solution[i].dominance} (expected {buildings_solution[i].dominance})')
             else:
                 print(f'PASSED: ({buildings_solution[i].coordinates[0]}, {buildings_solution[i].coordinates[1]}) : {buildings_my_solution[i].dominance}')
         
+        print(f'CASE SUMMARY: {f_count} / {total_count} ({f_count * 100 / total_count} % PASSED) - CASE {"FAILED" if f_count > 0 else "PASSED"}')
+        if f_count > 0:
+            total_fail += 1
+
+    print('===============================================================================')
+    print(f'TEST SUMMARY: {total_fail} / {number_of_random_tries} ({total_fail * 100 / number_of_random_tries} % CASE FAILED)')
